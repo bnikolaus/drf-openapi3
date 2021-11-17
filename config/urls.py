5f88928d00d97d4a4c9e8ce2a36cc0ea126ae4c7
Brent Nikolaus <bnikolaus@gmail.com>
2021-11-16 18:01:34 -0800

    testing out DRF with OpenAPI3

---
+++ config/urls.py
@@ -0,0 +1,26 @@
+from django.urls import include, path
+from django.contrib import admin
+from rest_framework import routers
+from restapi import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, login URLs included for the browsable API.
+urlpatterns = [
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls), 
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # OpenAPI 3 documentation with Swagger UI
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(
+            template_name="swagger-ui.html", url_name="schema"
+        ),
+        name="swagger-ui",
+    ),
+]
